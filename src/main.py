a334d6791523c7838bb7216f7419999cc9e72d56
ShanmingLi <64591@DESKTOP-AO4JHQD>
2017-03-01 20:29:16 +0000

    software_a3_parseinput

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 import urllib.request
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', help='input help')
+args = parser.parse_args()
+filename = args.input
 
 def read_url(filename):
     if filename.startswith('http'):
         return urllib.request.urlopen(filename).read().decode('UTF-8')
     else:
         return open(filename).read()
-
-buffer = read_url("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
-
-lines = buffer.splitlines()
-
-size = int(lines[0])
-print("Size is:",size)
 
 '''function that can make two-dimension list '''    
 def a2d(size):
@@ -59,7 +58,45 @@ def check_border(n, size):
         n = n
     return n
 
+'''read the file from the input over command line '''
+buffer = read_url(filename)
 
+lines = buffer.splitlines()
+
+size = int(lines[0])
+print("Size is:",size)
+
+a2d = a2d(size)
+for instructions in lines[1:]:
+    instruction = instructions.replace(',',' ').split()
+    
+    if instruction[0] == "turn" and (len(instruction) == 7):
+        x1 = (int) (instruction[2])
+        y1 = (int) (instruction[3])
+        x2 = (int) (instruction[5])
+        y2 = (int) (instruction[6])
+    elif instruction[0] == "switch" and (len(instruction) == 6):
+        x1 = (int) (instruction[1])
+        y1 = (int) (instruction[2])
+        x2 = (int) (instruction[4])
+        y2 = (int) (instruction[5])
+    
+        
+    x1 = check_border(x1, size)
+    y1 = check_border(y1, size)
+    x2 = check_border(x2, size)
+    y2 = check_border(y2, size)
+    
+    
+    if instruction[0] == "turn" and (len(instruction) == 7):
+        if instruction[1] == "on":
+            turn_on(x1, y1, x2, y2, a2d)
+        elif instruction[1] == "off":
+            turn_off(x1, y1, x2, y2, a2d)
+    elif instruction[0] == "switch" and (len(instruction) == 6):
+        switch(x1, y1, x2, y2, a2d)
+       
+print(count_lighting(size, a2d))
 
 
 
