31bf790c199139cb0763daed5ab844193f902af5
ShanmingLi <64591@DESKTOP-AO4JHQD>
2017-03-01 14:34:35 +0000

    software_a3_functions

--- src/main.py
+++ src/main.py
@@ -12,11 +12,12 @@ lines = buffer.splitlines()
 
 size = int(lines[0])
 print("Size is:",size)
-    
+
+'''function that can make two-dimension list '''    
 def a2d(size):
     a2d = [[False] * size for _ in range(size)]
     return a2d
-
+'''function that count the number of True in the 2d list'''
 def count_lighting(size, a):
     num = 0
     for i in range(0, size):
@@ -24,17 +25,39 @@ def count_lighting(size, a):
             num += a[i][j]
     return num
 
+'''function that change the specific element from False to True in the range'''
 def turn_on (x1, y1, x2, y2, a):
     for row in range (x1, x2 + 1):
         for col in range (y1, y2 + 1):
             a[row][col] = True    
     return
 
+'''function that change the specific element from True to False in the range'''
 def turn_off (x1, y1, x2, y2, a):
     for row in range (x1, x2 + 1):
         for col in range (y1, y2 + 1):
             a[row][col] = False    
     return
+
+'''function that switch the specific element's value in the range'''
+def switch (x1, y1, x2, y2, a):
+    for row in range (x1, x2 + 1):
+        for col in range (y1, y2 + 1):
+            if a[row][col]:
+                a[row][col] = False
+            else:
+                a[row][col] = True  
+    return
+
+'''function that will check if the coordinates out index of the range'''
+def check_border(n, size):
+    if n < 0:
+        n = max(0, n)
+    elif n > size:
+        n = min(size-1, n)
+    else:
+        n = n
+    return n
 
 
 
--- test/test.py
+++ test/test.py
@@ -23,9 +23,24 @@ def test_turn_on():
     count_lighting(4, array)
     eq_(count_lighting(4, array), 4, "Error with turn_on function")
     
-
+def test_switch():
+    array = [[True] * 4 for _ in range(4)]
+    a1, b1, a2, b2 = 1, 1, 2, 2
+    switch(a1, b1, a2, b2, array)
+    count_lighting(4, array)
+    eq_(count_lighting(4, array), 12, "Error with switch function")
     
+def test_check_border1():
+    size = 1000
+    x = -169
+    check_border(x, size)
+    eq_(check_border(x, size), 0, "Error with check_border function")
     
+def test_check_border2():
+    size = 1000
+    x = 1996
+    check_border(x, size)
+    eq_(check_border(x, size), 999, "Error with check_border function")    
     
     
     
